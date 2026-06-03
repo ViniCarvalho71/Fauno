97f1699600f632ea57417a0753d99d068bbff277
Marcos Gabriel de Paula Martins <86901372+MarcosGabMartins@users.noreply.github.com>
2026-06-03 19:21:44 -0300

    feat: criando Pet Entities e Repositories

---
+++ Fauno.Register/Fauno.Register/Fauno.Register.Domain/Repositories/IPetRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Fauno.Register.Domain.Entities;
+
+namespace Fauno.Register.Domain.Repositories;
+
+public interface IPetRepository
+{
+    Task SalvarAsync(Pet pet);
+    Task AtualizarAsync(Pet pet);
+    Task<Pet?> BuscarPorIdAsync(Guid id);
+    Task<IEnumerable<Pet>> BuscarPorDonoIdAsync(Guid donoId);
+}
